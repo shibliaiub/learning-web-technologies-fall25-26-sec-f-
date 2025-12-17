7454727ee2d8a408adc2835f2b89d66758176b6c
Mehedi Hasan Shibli <shibli2506@gmail.com>
2025-12-17 10:48:08 +0600

    php intro

---
+++ hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+// echo "Hello";
+// //same as:
+// echo("Hello");
+
+
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+
+?>
+</body>
+</html>
