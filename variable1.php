a8f07e73a5769192b0ee25f6c99004b425829149
Mehedi Hasan Shibli <shibli2506@gmail.com>
2025-12-17 11:10:10 +0600

    variables declaration

---
+++ variable1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;      // $x is an integer
+$y = "John"; // $y is a string
+
+echo $x;
+echo $y;
+
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+
+// <?php
+// $x = "John";
+// echo $x;
+// ?>
+
+// </body>
+// </html>
+
+
+?>
+
+</body>
+</html>
