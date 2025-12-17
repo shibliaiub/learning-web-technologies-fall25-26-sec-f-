d84a512e776aba8670a5609c860bff012b20ca3c
Mehedi Hasan Shibli <shibli2506@gmail.com>
2025-12-17 11:09:13 +0600

    variable using php

---
+++ variable.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+// $x = 5;
+// $y = "Shibli";
+
+//  $txt = "W3Schools.com"; 
+// echo "I love $txt!";
+
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+
+
+
+
+
+
+?>
+
+
+
+</body>
+</html>
